a16e483238f8188b0977e7befe34afc72084f119
Irvan Wijaya <irvanwijaya72@gmail.com>
2022-06-25 10:21:53 +0700

    membuat fungsi createDate di file index.js

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, fnSort) => {
+  sortData = fnSort(arr);
+
+  let data = []
+  for( let i=0; i<sortData.length; i++){
+    let loop=''
+    loop += i + 1 + '. ' + sortData[i]
+    data.push(loop)
+  }
+return data
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr)=>{
+  let data= arr.sort()
+  return data;
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr)=>{
+  let data = arr.sort().reverse()
+  return data
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
